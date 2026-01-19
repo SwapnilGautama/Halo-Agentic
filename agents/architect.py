a682bc8251e603c5305c9b7738d6741b75c4d81e
SwapnilGautama <swapnil.gautama@gmail.com>
2026-01-19 19:41:39 +0530

    Update architect.py

--- agents/architect.py
+++ agents/architect.py
@@ -1,7 +1,6 @@
 import pandas as pd
-from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
 import json
+from langchain_openai import ChatOpenAI
 
 class ArchitectAgent:
     def __init__(self, kpi_directory_path, prompt_path, model):
@@ -12,25 +11,24 @@ class ArchitectAgent:
             self.prompt_template = f.read()
 
     def run(self, user_query: str):
-        # ✅ IMPORTANT: Explicitly include KPI_Synonyms
-        kpi_context_df = self.kpi_df[[
-            "KPI_ID",
-            "KPI_Name",
-            "KPI_Synonyms",
-            "Business_Question",
-            "KPI_Category"
-        ]]
+        # Explicitly include KPI_Synonyms
+        kpi_context_df = self.kpi_df[
+            [
+                "KPI_ID",
+                "KPI_Name",
+                "KPI_Synonyms",
+                "Business_Question",
+                "KPI_Category",
+            ]
+        ]
 
         kpi_context = kpi_context_df.to_csv(index=False)
 
-        prompt = PromptTemplate(
-            input_variables=["KPI_DIRECTORY", "USER_QUESTION"],
-            template=self.prompt_template
-        )
-
-        final_prompt = prompt.format(
-            KPI_DIRECTORY=kpi_context,
-            USER_QUESTION=user_query
+        # Build prompt manually (NO PromptTemplate)
+        final_prompt = self.prompt_template.replace(
+            "{{KPI_DIRECTORY}}", kpi_context
+        ).replace(
+            "{{USER_QUESTION}}", user_query
         )
 
         response = self.model.invoke(final_prompt)
